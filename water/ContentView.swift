9f4015279cd06d8d58a82d48398b2b9b11e6c4aa
Haya Fahad <hayafahad46@gmail.com>
2023-10-08 01:46:35 +0300

    Initial Commit

---
+++ water/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  water
+//
+//  Created by Haya Fahad Alsabr on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ water/waterApp.swift
@@ -0,0 +1,17 @@
+//
+//  waterApp.swift
+//  water
+//
+//  Created by Haya Fahad Alsabr on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct waterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
